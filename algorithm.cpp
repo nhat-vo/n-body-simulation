0a4f9f4bec75aecf2ac08db11504ac0bcc0a3e8b
nhat.vo <tonystark0243@gmail.com>
2023-05-14 13:01:49 +0200

    move visualization to separate thread

--- algorithm.cpp
+++ algorithm.cpp
@@ -4,6 +4,7 @@
 #include "Magick++/STL.h"
 #include <Magick++.h>
 #include <cmath>
+#include <condition_variable>
 #include <format>
 #include <iostream>
 #include <mutex>
@@ -83,45 +84,66 @@ void update_positions(std::vector<Body *> &bodies, int start, int end,
         bodies[i]->r = bodies[i]->r + bodies[i]->v * dt;
 }
 
-// void simulate(std::vector<Body> &bodies, int num_threads, double dt,
-//               int num_steps) {
-//     int n = bodies.size();
-//     std::vector<std::thread> threads(num_threads);
-//     for (int step = 0; step < num_steps; step++) {
-//         // Compute forces
-//         int chunk_size = n / num_threads;
-//         int start = 0;
-//         int end = chunk_size;
-//         for (int i = 0; i < num_threads; i++) {
-//             if (i == num_threads - 1) {
-//                 end = n;
-//             }
-//             threads[i] =
-//                 std::thread(compute_forces, ref(bodies), start, end, dt);
-//             start = end;
-//             end += chunk_size;
-//         }
-//         for (auto &t : threads) {
-//             t.join();
-//         }
+void simulate(std::vector<Body *> &bodies, int num_threads, double dt,
+              int num_steps) {
+    int n = bodies.size();
+    std::vector<std::thread> threads(num_threads);
+    for (int step = 0; step < num_steps; step++) {
+        // Compute forces
+        int chunk_size = n / num_threads;
+        int start = 0;
+        int end = chunk_size;
+        for (int i = 0; i < num_threads; i++) {
+            if (i == num_threads - 1) {
+                end = n;
+            }
+            threads[i] =
+                std::thread(compute_forces, ref(bodies), start, end, dt);
+            start = end;
+            end += chunk_size;
+        }
+        for (auto &t : threads) {
+            t.join();
+        }
 
-//         // Update positions
-//         start = 0;
-//         end = chunk_size;
-//         for (int i = 0; i < num_threads; i++) {
-//             if (i == num_threads - 1) {
-//                 end = n;
-//             }
-//             threads[i] =
-//                 std::thread(update_positions, ref(bodies), start, end, dt);
-//             start = end;
-//             end += chunk_size;
-//         }
-//         for (auto &t : threads) {
-//             t.join();
-//         }
-//     }
-// }
+        // Update positions
+        start = 0;
+        end = chunk_size;
+        for (int i = 0; i < num_threads; i++) {
+            if (i == num_threads - 1) {
+                end = n;
+            }
+            threads[i] =
+                std::thread(update_positions, ref(bodies), start, end, dt);
+            start = end;
+            end += chunk_size;
+        }
+        for (auto &t : threads) {
+            t.join();
+        }
+    }
+}
+
+std::mutex draw_lock;
+std::condition_variable draw_signal;
+const Vect canvas_size{800, 500};
+
+void draw(std::vector<Magick::Image> &images, const std::vector<Body *> &bodies,
+          double &draw_t, double &current_t, double end_t) {
+    std::unique_lock<std::mutex> lock(draw_lock);
+    while (draw_t < end_t) {
+        while (draw_t < current_t)
+            draw_signal.wait(lock);
+        Magick::Image frame(Magick::Geometry(canvas_size.x, canvas_size.y),
+                            Magick::Color("white"));
+        for (const Body *body : bodies) {
+            frame.fillColor(body->color);
+            frame.draw(Magick::DrawableCircle(body->r.x, body->r.y,
+                                              body->r.x - 10, body->r.y));
+        }
+        images.push_back(frame);
+    }
+}
 
 int main(int argc, char **argv) {
     Magick::InitializeMagick(*argv);
@@ -133,13 +155,12 @@ int main(int argc, char **argv) {
     int end = 2;
 
     // double vy = sqrt(G * 1e14 / 100);
-    const Vect canvas_size{800, 500};
     const Vect offset = canvas_size / 2;
     double vy = 0;
     Body b1 = Body(1e14, offset + Vect{0, 0}, {0, vy}, "blue");
     Body b2 = Body(1e14, offset + Vect{100, 0}, {0, -vy}, "red");
     Body b3 =
-        Body(1, offset + Vect{0, -50}, {sqrt(2. * G * 1e14 / 50), 0}, "green");
+        Body(1, offset + Vect{0, -50}, {sqrt(1.2 * G * 1e14 / 50), 0}, "green");
     std::cout << "body created" << std::endl;
 
     std::vector<Body *> bodies{&b1, &b3};
@@ -148,22 +169,20 @@ int main(int argc, char **argv) {
     double gif_dt = 1;
     double gif_t = 0;
 
-    for (double t = dt; t <= t_end; t += dt) {
+    double t = 0;
+
+    std::thread draw_thread(draw, std::ref(images), std::ref(bodies),
+                            std::ref(gif_t), std::ref(t), t_end);
+
+    for (; t <= t_end; t += dt) {
         compute_forces(bodies, start, end, dt);
         update_positions(bodies, start, end, dt);
-        // std::cout << t << ": " << *bodies[0] << " " << *bodies[1] <<
-        // std::endl;
         if (t >= gif_t) {
-            Magick::Image frame(Magick::Geometry(canvas_size.x, canvas_size.y),
-                                Magick::Color("white"));
-            for (const Body *body : bodies) {
-                frame.fillColor(body->color);
-                frame.draw(Magick::DrawableCircle(body->r.x, body->r.y,
-                                                  body->r.x - 10, body->r.y));
-            }
-            images.push_back(frame);
+            std::lock_guard<std::mutex> _lk(draw_lock);
             gif_t += gif_dt;
+            draw_signal.notify_one();
         }
     }
+    draw_thread.join();
     Magick::writeImages(images.begin(), images.end(), "image.gif");
 }
