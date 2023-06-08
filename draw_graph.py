2d6e3a4ba2df2e1fa0e1a8a10d3dbc4dbd6e6b4f
vrushank-agrawal <vrushank2001@gmail.com>
2023-06-08 20:42:30 +0200

    add more graphs

--- draw_graph.py
+++ draw_graph.py
@@ -42,7 +42,7 @@ def error_graphs():
     error = np.sum((data_single - data_barnes)**2, axis=1) / len(data_single)
     error_max = np.max((data_single - data_barnes)**2, axis=1)
 
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(6, 6))
     plt.plot(error, label="error")
     plt.xlabel("Time")
     plt.ylabel("Error")
@@ -50,7 +50,7 @@ def error_graphs():
     plt.savefig("graphs/error.png")
 
     # plot the graph
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(6, 6))
     plt.plot(distance_single, label="single")
     plt.plot(distance_barnes, label="barnes")
     plt.xlabel("Time")
@@ -59,7 +59,7 @@ def error_graphs():
     plt.savefig("graphs/distance.png")
 
     # plot the graph
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(6, 6))
     plt.plot(error_max, label="error_max")
     plt.xlabel("Time")
     plt.ylabel("Error")
@@ -75,7 +75,7 @@ def simulation_time_graphs():
 
     body_pos = {0: 5, 1: 50, 2: 500, 3: 5000}
     for body in range(4):
-        plt.figure(figsize=(10, 10))
+        plt.figure(figsize=(6, 6))
         plt.plot(threads, algo_data[0, body, :], label="single")
         plt.plot(threads, algo_data[1, body, :], label="multi-1")
         plt.plot(threads, algo_data[2, body, :], label="multi-2")
@@ -88,7 +88,30 @@ def simulation_time_graphs():
         plt.legend()
         plt.savefig("graphs/simulation_time_{}.png".format(body_pos[body]))
 
+def thread_algo_graph():
+    data = np.loadtxt("thread_algo.txt", dtype=float, delimiter=" ")
+    multi_algo_data = np.array([data[:4, 1:], data[4:8, 1:], data[8:12, 1:]])
 
+    # print(multi_algo_data[1, ])
+    # return
+    threads = [1, 2, 4, 8]
+
+    algo_names = {0: "multi-1", 1: "multi-2", 2: "barnes-multi"}
+    for algo in range(3):
+        plt.figure(figsize=(6, 6))
+        plt.plot(threads, multi_algo_data[algo, 0, :], label="5 bodies")
+        plt.plot(threads, multi_algo_data[algo, 1, :], label="50 bodies")
+        plt.plot(threads, multi_algo_data[algo, 2, :], label="500 bodies")
+        if algo == 2:
+            plt.plot(threads, multi_algo_data[algo, 3, :], label="5000 bodies")
+        plt.xlabel("Threads")
+        plt.ylabel("Time")
+        plt.title("{} algorithm".format(algo_names[algo]))
+        # plt.yscale("log")
+        plt.legend()
+        plt.savefig("graphs/thread_algo_{}.png".format(algo_names[algo]))
 
 if __name__ == "__main__":
+    # thread_algo_graph()
     simulation_time_graphs()
+    # error_graphs()
