f1af8c438dbe906ca974435e50e29d76f695c145
nhat.vo <tonystark0243@gmail.com>
2023-06-08 19:15:08 +0200

    update readme

--- benchmark.sh
+++ benchmark.sh
@@ -1,10 +1,10 @@
-make main
+make benchmark
 for n_threads in 2 4 8; do
-    for n_bodies in 5 50 500 5000; do
-        for algo in 1 2 4; do
-            ./main $algo $n_threads $n_bodies
-        done
-        echo ""
-    done
-    echo ""
+	for n_bodies in 5 50 500 5000; do
+		for algo in 1 2 4; do
+			./main $algo $n_threads $n_bodies
+		done
+		echo ""
+	done
+	echo ""
 done
