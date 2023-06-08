9873edafc98b9acb1bead4b582cdca21b91a2a62
vrushank-agrawal <vrushank2001@gmail.com>
2023-06-08 14:51:41 +0200

    add graphs for simulation time

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,7 @@
 make main
-for n_threads in 1 2 4 8; do
+for n_threads in 2 4 8; do
     for n_bodies in 5 50 500 5000; do
-        for algo in 0 1 2 3 4; do
+        for algo in 1 2 4; do
             ./main $algo $n_threads $n_bodies
         done
         echo ""
--- draw_graph.py
+++ draw_graph.py
@@ -28,7 +28,7 @@ def parse(f):
     return np.array(data)
 
 
-if __name__ == "__main__":
+def error_graphs():
     # parse the output.txt file
     data_single = parse("single.txt")
     data_barnes = parse("barnes.txt")
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     plt.xlabel("Time")
     plt.ylabel("Error")
     plt.legend()
-    plt.savefig("error.png")
+    plt.savefig("graphs/error.png")
 
     # plot the graph
     plt.figure(figsize=(10, 10))
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     plt.xlabel("Time")
     plt.ylabel("Distance")
     plt.legend()
-    plt.savefig("distance.png")
+    plt.savefig("graphs/distance.png")
 
     # plot the graph
     plt.figure(figsize=(10, 10))
@@ -64,4 +64,31 @@ if __name__ == "__main__":
     plt.xlabel("Time")
     plt.ylabel("Error")
     plt.legend()
-    plt.savefig("error_max.png")
+    plt.savefig("graphs/error_max.png")
+
+def simulation_time_graphs():
+    data = np.loadtxt("simulation_time.txt", dtype=float, delimiter=" ")
+    algo_data = np.array([data[:4, 1:], data[4:8, 1:], data[8:12, 1:], data[12:16, 1:], data[16:20, 1:]])
+
+    # print(algo_data[1, 3, :])
+    threads = [1, 2, 4, 8]
+
+    body_pos = {0: 5, 1: 50, 2: 500, 3: 5000}
+    for body in range(4):
+        plt.figure(figsize=(10, 10))
+        plt.plot(threads, algo_data[0, body, :], label="single")
+        plt.plot(threads, algo_data[1, body, :], label="multi-1")
+        plt.plot(threads, algo_data[2, body, :], label="multi-2")
+        plt.plot(threads, algo_data[3, body, :], label="barnes")
+        plt.plot(threads, algo_data[4, body, :], label="barnes-multi")
+        plt.xlabel("Threads")
+        plt.ylabel("Time")
+        plt.title("Simulation Time for {} bodies".format(body_pos[body]))
+        # plt.yscale("log")
+        plt.legend()
+        plt.savefig("graphs/simulation_time_{}.png".format(body_pos[body]))
+
+
+
+if __name__ == "__main__":
+    simulation_time_graphs()
